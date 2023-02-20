63627ea17ad1106b794a7f6e46b8182685a605bb
Lucas <lluc23@hotmail.com>
2023-02-20 21:42:59 +0100

    mod

--- src-tauri/src/db/mod.rs
+++ src-tauri/src/db/mod.rs
@@ -2,8 +2,8 @@ pub mod commands;
 pub mod crud;
 pub mod models;
 pub mod operations;
-pub mod pros_cons;
 
-pub use notes::*;
+pub use commands::*;
+pub use crud::*;
+pub use models::*;
 pub use operations::*;
-pub use pros_cons::*;
