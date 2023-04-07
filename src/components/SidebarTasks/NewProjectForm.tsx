0c5abe14088c65003b242f52cc975e408c41d246
Lucas <lluc23@hotmail.com>
2023-04-07 12:58:14 +0200

    no project with no name

--- src/components/SidebarTasks/NewProjectForm.tsx
+++ src/components/SidebarTasks/NewProjectForm.tsx
@@ -1,14 +1,14 @@
 import { IconButton } from "@/components/Button";
 import { Plus } from "@/components/Icons";
-import {Project} from "@/recoil/tasks/types"
+import { Project } from "@/recoil/tasks/types"
 
 import { FormEvent, useState } from "react";
 import { useRecoilState, RecoilState } from "recoil";
 import { v4 as uuid } from "uuid";
 
 type Props = {
-    recoilStateMethod: RecoilState<any>
-  };
+  recoilStateMethod: RecoilState<any>
+};
 
 export const NewProjectForm = ({ recoilStateMethod }: Props) => {
   const [tempName, setTempName] = useState("");
@@ -22,26 +22,28 @@ export const NewProjectForm = ({ recoilStateMethod }: Props) => {
 
   const createItem = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const newObj = items.reduce((newItem: object, currentItem: Project) => {
-      newItem[currentItem.id] = currentItem;
-      return newItem;
-    }, {});
-    const id = uuid();
-    newObj[id] = {id:id, name:tempName, tasks: {}} as Project;
-    setItems(newObj);
-    resetForm();
+    if (tempName != "") {
+      const newObj = items.reduce((newItem: object, currentItem: Project) => {
+        newItem[currentItem.id] = currentItem;
+        return newItem;
+      }, {});
+      const id = uuid();
+      newObj[id] = { id: id, title: tempName, tasks: {} } as Project;
+      setItems(newObj);
+      resetForm();
+    }
   };
 
-    return (
-<form className="self-center w-full p-3" onSubmit={createItem}>
-    <div className="flex">
+  return (
+    <form className="self-center w-full p-3" onSubmit={createItem}>
+      <div className="flex">
         <div className="relative w-full">
-            <input 
-                type="text"
-                value={tempName}
-                onChange={handleChange}
-                id="search-dropdown" 
-                className="
+          <input
+            type="text"
+            value={tempName}
+            onChange={handleChange}
+            id="search-dropdown"
+            className="
                     block p-2.5 w-full 
                     z-20 text-sm text-gray-900 
                     bg-gray-50 rounded-lg 
@@ -53,11 +55,11 @@ export const NewProjectForm = ({ recoilStateMethod }: Props) => {
                     dark:border-gray-600 
                     dark:placeholder-gray-400 
                     dark:text-white 
-                    dark:focus:border-blue-500" 
-                placeholder="New" />
-            <button 
-                type="submit" 
-                className="
+                    dark:focus:border-blue-500"
+            placeholder="New" />
+          <button
+            type="submit"
+            className="
                     absolute top-0 right-0 
                     p-2.5 text-sm font-medium 
                     text-white bg-blue-700 
@@ -67,12 +69,12 @@ export const NewProjectForm = ({ recoilStateMethod }: Props) => {
                     focus:ring-blue-300 dark:bg-blue-600 
                     dark:hover:bg-blue-700 
                     dark:focus:ring-blue-800">
-                <Plus size={20} />
-            </button>
+            <Plus size={20} />
+          </button>
         </div>
-    </div>
-</form>
-    );
+      </div>
+    </form>
+  );
 };
 
 
